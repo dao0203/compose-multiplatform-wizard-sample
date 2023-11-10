45b03304e965e1cd9eb0fd0d96d2707c76d1357e
dao0203 <youzaizuoteng872@gmail.com>
2023-11-10 14:03:16 +0900

    create function for returning todaysDate

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -12,6 +12,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -36,4 +43,13 @@ fun App() {
             }
         }
     }
+}
+
+fun todaysDate(): String {
+    fun LocalDateTime.format() = toString().substringBefore("T")
+
+    val now = Clock.System.now()
+    val zone = TimeZone.currentSystemDefault()
+
+    return now.toLocalDateTime(zone).format()
 }
